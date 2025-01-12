6ac5e2c603bc27eeed4b40e0b07494829415eff0
Deepak Mathpal <deepak.m@safe.security>
2025-01-12 16:29:05 +0530

    Update noti.py

--- python_files/noti.py
+++ python_files/noti.py
@@ -16,7 +16,51 @@ def main():
         summary = "service deployment"
 
     
-    return {
+  #   return {
+  # "fields": {
+  #   "project": {
+  #     "id": "10113"
+  #   },
+  #   "assignee": {
+  #     "id": "712020:89e17fe4-7635-42c7-bdb7-11ab1f4c7dc8"
+  #   },
+  #   "summary": summary,
+  #   "issuetype": {
+  #     "id": "10148"
+  #   },
+  #   "customfield_10166": {
+  #     "type": "doc",
+  #     "version": 1,
+  #     "content": [
+  #       {
+  #         "type": "paragraph",
+  #         "content": [
+  #           {
+  #             "type": "text",
+  #             "text": "Deploy following stacks using the job - Independent Services:- https://jenkins.lucideus.tech/job/CloudOps/job/Production/job/(Safe One) Independent Service Upgrade/"
+  #           }
+  #         ]
+  #       }
+  #     ]
+  #   },
+  #   "customfield_10167": {
+  #     "type": "doc",
+  #     "version": 1,
+#       "content": [
+#         {
+#           "type": "paragraph",
+#           "content": [
+#             {
+#               "type": "text",
+#               "text": "Use the same job for rolling back to the previous version, JOB ----> https://jenkins.lucideus.tech/job/CloudOps/job/Production/job/(Safe One) Independent Service Upgrade/"
+#             }
+#           ]
+#         }
+#       ]
+#     }
+#   }
+# }
+    payload_data = {
   "fields": {
     "project": {
       "id": "10113"
@@ -60,6 +104,7 @@ def main():
     }
   }
 }
+    print(payload_data)
 
 
 if __name__ == "__main__":
